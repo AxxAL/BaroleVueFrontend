ec97e0000dd037cbebc0ecf56464293768cd3b80
Axel <ageinthehouse@gmail.com>
2022-02-19 02:37:06 +0100

    Base Push

---
+++ src/components/RoleList.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    data() {
+        return {
+            roles: null
+        }
+    },
+
+  created() {
+    this.fetchRoles();
+  },
+
+  methods: {
+    async fetchRoles() {
+      await fetch("https://localhost:7109/api/v1/role/all")
+        .then(async response => {
+            this.roles = await response.json();
+            console.log(this.roles);
+        })
+        .catch(error => console.error(error));
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+        <ul>
+            <h2>Role list:</h2>
+            <li v-for="role in roles" :key="role.Id">
+                Title: {{ role.title }}<br>
+                Job: {{ role.barotraumaJob }}
+            </li>
+        </ul>
+  </main>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script>
+import RoleList from "../components/RoleList.vue"
+export default {
+  components: {
+    RoleList
+  }
+}
+</script>
+
+<template>
+  <RoleList />
+</template>
+
+<style>
+
+</style>
